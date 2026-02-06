33dc1261405b1628a1afd01d67fd6b19be24ea92
Lakshmi Kusuma <nimmy@macbookpro.mynetworksettings.com>
2026-02-06 14:04:05 -0500

    pushing reviewed version of driver factory, hooks and configreader

--- src/test/java/factory/Driverfactory.java
+++ src/test/java/factory/Driverfactory.java
@@ -1,5 +1,48 @@
 package factory;
 
-public class Driverfactory {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import utilities.ConfigReader;
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+
+    // Prevent object creation
+    private DriverFactory() {}
+
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+
+            String browser = ConfigReader.getProperty("browser");
+
+            switch (browser.toLowerCase()) {
+                case "firefox":
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "chrome":
+                default:
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }

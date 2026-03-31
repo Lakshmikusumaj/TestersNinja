1828b22683c4e8e9040a7464d6c03762b9fa6df8
Ravali Veerla <ravaliveerla@Ravalis-MacBook-Pro.local>
2026-03-30 20:43:59 -0500

    update testrunner

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -12,14 +12,14 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	features = "src/test/resources/features",
 	glue = { "stepDefinitions", "hooks" },
-	tags = "@Login", 
+	tags = "@Array", 
 	plugin = { "pretty", 
 			  "html:target/cucumber-reports/cucumber.html",
 			  // "json:target/cucumber-reports/cucumber.json",
 			  //"com.aventstack.chaintest.plugins.ChainTestCucumberListener:target/chaintest-report",
 			  "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 			  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-			}, 
+			},
 	monochrome = true, 
 	publish = true
 )
